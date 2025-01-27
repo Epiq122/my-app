12840e839cf1b3490f21cbf6848831d6d7cfb856
Robert Gleason <l2ob122@gmail.com>
2025-01-26 21:39:01 -0800

    Using pipes added 2 new fields to the product a price and category and got them to display on the page

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ViewChild, Component, viewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { Product } from '../product';
 
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
@@ -9,23 +9,47 @@ import { ProductDetailComponent } from '../product-detail/product-detail.compone
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
 })
-export class ProductListComponent implements AfterViewInit {
+export class ProductListComponent {
   products: Product[] = [
-    { id: 1, title: 'Keyboard' },
-    { id: 2, title: 'Microphone' },
-    { id: 3, title: 'Web Camera' },
-    { id: 4, title: 'Tablet' },
+    {
+      id: 1,
+      title: 'Keyboard',
+      price: 100,
+      categories: {
+        1: 'Computing',
+        2: 'Peripherals',
+      },
+    },
+    {
+      id: 2,
+      title: 'Microphone',
+      price: 35,
+      categories: {
+        3: 'Multimedia',
+      },
+    },
+    {
+      id: 3,
+      title: 'Web Camera',
+      price: 79,
+      categories: {
+        1: 'Computing',
+        3: 'Multimedia',
+      },
+    },
+    {
+      id: 4,
+      title: 'Tablet',
+      price: 500,
+      categories: {
+        4: 'Entertainment',
+      },
+    },
   ];
 
   selectedProduct: Product | undefined = this.products[0];
 
   onAdded(product: Product) {
     alert(`${this.selectedProduct?.title} added to cart`);
-  }
-
-  productDetail = viewChild(ProductDetailComponent);
-
-  ngAfterViewInit(): void {
-    console.log(this.productDetail()!.product());
   }
 }
